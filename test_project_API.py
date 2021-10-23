333fc23f45a02eeea13e7362f4f870c60b8ccce4
Pakarat Kongpaisan <pakarat@MBPkhxngPakarat.ST-244F>
2021-10-23 23:44:15 +0700

    test api of reservation id

---
+++ test_project_API.py
@@ -0,0 +1,43 @@
+"""Test project API of government group's name is Flamxby."""
+import unittest
+import requests
+
+
+class TestApi(unittest.TestCase):
+    """Class for test webside status when Gets the specific reservations
+    detail from reservation id."""
+
+    def test_check_status_code_equals_200(self):
+        """This function test that the website can GET reservation id to
+        succeed."""
+        self.response = requests.get("https://flamxby.herokuapp.com/reservation/5")
+        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual("application/json", self.response.headers["Content-Type"])
+
+    def test_reservation_id_are_get_correct_information(self):
+        """This function test that the input reservation id and get
+        information on that reservation id."""
+        self.response = requests.get("https://flamxby.herokuapp.com/reservation/5")
+        self.assertEqual(self.response.json()["reservation_id"], 5)
+
+    def test_reservation_id_input_negative_number(self):
+        """This function test that it should not found information when the input
+        reservation id is a negative number."""
+        self.response = requests.get("https://flamxby.herokuapp.com/reservation/-1")
+        self.assertEqual(self.response.status_code, 404)
+        self.assertEqual(
+            self.response.json(), {"detail": "No reservation with this id"}
+        )
+
+    def test_reservation_id_input_not_exist(self):
+        """This function test that it should not found information when the input
+        reservation id is not exist."""
+        self.response = requests.get("https://flamxby.herokuapp.com/reservation/500000")
+        self.assertEqual(self.response.status_code, 404)
+        self.assertEqual(
+            self.response.json(), {"detail": "No reservation with this id"}
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
