b2a6fc9aefae45c748ba3caac11bb86fae506bd7
Pakarat Kongpaisan <pakarat@MacBook-Pro-khxng-Pakarat.local>
2021-11-18 12:56:12 +0700

    Improve API tests

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Test-API-Government-Flamxby-group
+# vaccine-api-test of Flamxby group
+
+## Endpoint
+- testing when `Get` the specific reservations detail from reservation id.
+
+## Testcast
+| List of test cases | Name                                                 | Description                                                                                                  | Result |
+|--------------------|------------------------------------------------------|--------------------------------------------------------------------------------------------------------------|--------|
+| 1                  | test_reservation_id_succeed                          | This function test that the website can GET reservation id to succeed.                                       | pass   |
+| 2                  | test_reservation_id_input_negative_number            | This function test that it should not find information when the input a reservation id is a negative number. | pass   |
+| 3                  | test_reservation_id_input_not_exist                  | This function test that it should not find information when the input reservation id does not exist.         | pass   |
+| 4                  | test_reservation_id_are_get_correct_response_body    | This function test that the input reservation id and get information on that reservation id.                 | pass   |
+| 5                  | test_reservation_id_are_get_correct_response_headers | This function test that the input reservation id and get correct response headers.                           | pass   |
--- test_project_API.py
+++ test_project_API.py
@@ -4,21 +4,15 @@ import requests
 
 
 class TestApi(unittest.TestCase):
-    """Class for test webside status when Gets the specific reservations
+    """Class for test when Gets the specific reservations
     detail from reservation id."""
 
-    def test_check_status_code_equals_200(self):
+    def test_reservation_id_succeed(self):
         """This function test that the website can GET reservation id to
         succeed."""
         self.response = requests.get("https://flamxby.herokuapp.com/reservation/5")
         self.assertEqual(self.response.status_code, 200)
         self.assertEqual("application/json", self.response.headers["Content-Type"])
-
-    def test_reservation_id_are_get_correct_information(self):
-        """This function test that the input reservation id and get
-        information on that reservation id."""
-        self.response = requests.get("https://flamxby.herokuapp.com/reservation/5")
-        self.assertEqual(self.response.json()["reservation_id"], 5)
 
     def test_reservation_id_input_negative_number(self):
         """This function test that it should not found information when the input
@@ -37,6 +31,18 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             self.response.json(), {"detail": "No reservation with this id"}
         )
+
+    def test_reservation_id_are_get_correct_response_body(self):
+        """This function test that the input reservation id and get
+        information on that reservation id."""
+        self.response = requests.get("https://flamxby.herokuapp.com/reservation/5")
+        self.assertEqual(self.response.json()["reservation_id"], 5)
+
+    def test_reservation_id_are_get_correct_response_headers(self):
+        """This function test that the input reservation id and get
+        correct response headers."""
+        self.response = requests.get("https://flamxby.herokuapp.com/reservation/5")
+        self.assertEqual("uvicorn", self.response.headers["server"])
 
 
 if __name__ == "__main__":
